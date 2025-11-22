6dbbded27580d4eb2609a15bfc52192d2cf49361
krtu0p <adiaksab99@gmail.com>
2025-11-23 00:06:11 +0800

    +finished noteService refactor comes later

--- internal/service/noteService.go
+++ internal/service/noteService.go
@@ -1,12 +1,13 @@
 package service
 
 import (
-	"strings"
 	"crypto/rand"
 	"fmt"
 	"graphql-pkm/internal/database"
 	"graphql-pkm/internal/models"
+	"strings"
 	"time"
+
 )
 
 type noteService struct {
@@ -28,7 +29,7 @@ func generateId() (string, error) {
 	return fmt.Sprintf("%x", b), nil
 }
 
-func (s *noteService) createNote(title, content string, tags []string) ([]*models.Note, error) {
+func (s *noteService) createNote(title, content string, tags []string) (*models.Note, error) {
 	if strings.TrimSpace(title) == "" {
 		return nil, fmt.Errorf("Tittle cannot be empty")
 	}
@@ -53,6 +54,60 @@ func (s *noteService) createNote(title, content string, tags []string) ([]*model
 		return nil, fmt.Errorf("failed to create note: %w", err)
 	}
 	
+	return note, nil
+	
+}
+
+func (s *noteService) GetAllNotes() ([]*models.Note, error) {
+	return s.db.GetAllNotes()
+}
+
+func (s *noteService) GetNote(id string) (*models.Note, error) {
+	return s.db.GetNote(id)
+}
+
+func (s *noteService) GetNotesByTags(tag string) ([]*models.Note, error) {
+	return  s.db.GetNotesByTag(tag)
+}
+
+func (s *noteService) UpdateNote(id string, title, content *string, tags []string) (*models.Note, error) {
+	note, err := s.db.GetNote(id)
+	if err != nil {
+		return nil, err
+	}
+	
+	if note == nil {
+		return nil, fmt.Errorf("note doesn't exist")
+	}
+	
+	if title != nil {
+		note.Title = strings.TrimSpace(*title)
+	}
+	
+	if content != nil {
+		note.Content = strings.TrimSpace(*content)
+	}
+	
+	if tags != nil {
+		note.Tags = s.normalizeTags(tags)
+	}
+	
+	note.UpdatedAt = time.Now()
+	
+	err = s.db.UpdateNote(note)
+	if err != nil {
+		return nil, err
+	}
+	
+	return note, nil
+}
+
+func (s *noteService) DeleteNote(id string) error {
+	return s.db.DeleteNote(id)
+}
+
+func (s *noteService) GetLinks(noteID string) ([]*models.Link, error) {
+	return s.db.GetLinksByNote(noteID)
 }
 
 func (s *noteService) normalizeTags(tags []string) []string {
@@ -63,4 +118,6 @@ func (s *noteService) normalizeTags(tags []string) []string {
 			normalized = append(normalized, strings.ToLower(trimmed))
 		}
 	}
+	
+	return normalized
 }
